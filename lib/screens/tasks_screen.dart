38f80e10af1290b79ede7fb01821e2b9ce244baf
dastagg:opus5 <dastagg@stagglinux.com>
2022-03-04 08:47:12 -0500

    For whatever reason, I can not seem to "get" Lifting State up to add the tasks to the main screen.
    And since the finished code isn't broken up into branches, I'm going to just save this as uncompleted and try to move on to the next phase of using Provider.

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -1,98 +1,90 @@
 import 'package:flutter/material.dart';
-
+import 'add_task_screen.dart';
 import '../models/task.dart';
-import '../screens/add_task_screen.dart';
 import '../widgets/tasks_list.dart';
 
 class TasksScreen extends StatefulWidget {
-  const TasksScreen({Key? key}) : super(key: key);
-
-
   @override
-  State<TasksScreen> createState() => _TasksScreenState();
+  _TasksScreenState createState() => _TasksScreenState();
 }
 
-class _TasksScreenState extends State<TasksScreen> {
-
-  List<Task> tasks = [
-    Task(name: 'Buy milk'),
-    Task(name: 'Buy eggs'),
-    Task(name: 'Buy bread'),
+class _TasksScreenState extends State {
+  List tasks = [
+    Task(name: 'Task name here', isDone: false),
+    Task(name: 'Task name here', isDone: false),
+    Task(name: 'Task name here', isDone: false)
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Colors.lightBlueAccent,
-        child: const Icon(Icons.add),
-        onPressed: () {
-          showModalBottomSheet(
-            context: context,
-            isScrollControlled: true,
-            builder: (context) => SingleChildScrollView(
-              padding: EdgeInsets.only(
-                  bottom: MediaQuery.of(context).viewInsets.bottom),
-              child: const AddTaskScreen(),
-            ),
-          );
-        },
-      ),
       backgroundColor: Colors.lightBlueAccent,
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            padding: const EdgeInsets.only(
-                top: 60.0, left: 30.0, right: 30.0, bottom: 30.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const CircleAvatar(
-                  child: Icon(
-                    Icons.list,
-                    size: 30.0,
-                    color: Colors.lightBlueAccent,
-                  ),
-                  backgroundColor: Colors.white,
-                  radius: 30.0,
+      floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.lightBlueAccent,
+          child: const Icon(Icons.add),
+          onPressed: () {
+            showModalBottomSheet(
+              context: context,
+              builder: (context) => AddTaskScreen(
+                addNewTask: (newTaskTitle) {
+                  setState(() {
+                    tasks.add(Task(name: newTaskTitle));
+                  });
+                  Navigator.pop(context);
+                },
+              ),
+            );
+          }),
+      body: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+        Padding(
+          padding: const EdgeInsets.only(top: 60, left: 30),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const CircleAvatar(
+                radius: 30,
+                backgroundColor: Colors.white,
+                child: Icon(
+                  Icons.list,
+                  size: 35,
                 ),
-                const SizedBox(
-                  height: 10.0,
-                ),
-                const Text(
-                  'Todoey',
-                  style: TextStyle(
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              const Text(
+                'Todoey',
+                style: TextStyle(
+                    fontSize: 50,
                     color: Colors.white,
-                    fontSize: 50.0,
-                    fontWeight: FontWeight.w700,
-                  ),
-                ),
-                Text(
-                  '${tasks.length} Tasks',
-                  style: const TextStyle(
-                    color: Colors.white,
-                    fontSize: 18,
-                  ),
-                ),
-              ],
+                    fontWeight: FontWeight.w700),
+              ),
+              Text(
+                '${tasks.length} tasks',
+                style: const TextStyle(fontSize: 15, color: Colors.white),
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+            ],
+          ),
+        ),
+        Expanded(
+          child: Container(
+            decoration: const BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(20),
+                topRight: Radius.circular(20),
+              ),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              // child: TaskList(tasks: tasks),
             ),
           ),
-          Expanded(
-            child: Container(
-                padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                height: 300.0,
-                decoration: const BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.only(
-                    topRight: Radius.circular(20.0),
-                    topLeft: Radius.circular(20.0),
-                  ),
-                ),
-                child: TasksList(tasks: tasks)),
-          ),
-        ],
-      ),
+        ),
+      ]),
     );
   }
 }
