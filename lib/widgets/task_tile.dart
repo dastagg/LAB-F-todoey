54eeca668a4ece876efed09ad4cd22b4216146d3
dastagg:opus5 <dastagg@stagglinux.com>
2022-03-04 10:55:42 -0500

    Add deleteTask.

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -4,17 +4,20 @@ class TaskTile extends StatelessWidget {
   final bool isChecked;
   final String taskTitle;
   final Function(bool?)? checkboxCallback;
+  final Function()? longPressCallback;
 
   const TaskTile(
-      {required this.isChecked,
-      required this.taskTitle,
-      required this.checkboxCallback,
+      { required this.isChecked,
+        required this.taskTitle,
+        required this.checkboxCallback,
+        required this.longPressCallback,
       Key? key})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
+      onLongPress: longPressCallback,
       title: Text(
         taskTitle,
         style: TextStyle(
--- lib/widgets/tasks_list.dart
+++ lib/widgets/tasks_list.dart
@@ -20,6 +20,9 @@ class TasksList extends StatelessWidget {
               checkboxCallback: (bool? checkboxState) {
                 taskData.updateTask(task);
               },
+              longPressCallback: () {
+                taskData.deleteTask(task);
+              },
             );
           },
           itemCount: taskData.taskCount,
